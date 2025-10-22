295852ccc6e9874f7fe68c4f01ca6235e7cba8ee
Ahsaan Ullah <ahsaanu70@gmail.com>
2025-10-22 00:20:17 -0700

    Set up basic error-handling middleware and handle MongoDB connection and redis connection

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,13 @@
 import {app} from './app';
+import dbConnection from "./utils/db";
 require("dotenv").config();
+
 
 
 // create server
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
+    // database connection
+    dbConnection();
 });
